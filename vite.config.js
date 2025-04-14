8b3e9d9d5358772852c63e87c8a086e5c860df8f
Greice Moreira <greice.f.d.a@hotmail.com>
2025-04-14 15:23:46 -0400

    paginas

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        form: resolve(__dirname, "src/form.html"),
+        gallery: resolve(__dirname, "src/gallery.html"),
+        colorPalette: resolve(__dirname, "src/colorPalette.html"),
+        thankyou: resolve(__dirname, "src/thankyou.html"),
       },
     },
   },
